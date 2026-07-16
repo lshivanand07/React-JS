c2eb3bce46a61781c6d88953e42978a71d2ffd38
Shivanandlamani <lshivanand07@gmail.com>
2026-07-16 11:29:20 +0530

    update env file

--- assignment/src/services/dashboardApi.ts
+++
@@ -1,7 +0,0 @@
-import api from './api';
-
-export async function fetchFlipkartRecords() {
-  console.log('hii')
-  const response = await api.get('/get-flipkart-records');
-  return response.data;
-}
